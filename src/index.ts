949edbbcfbe00a97e04dd62e86b23cc32d7c5424
Victor Carrillo <gatitto079@gmail.com>
2025-04-01 00:19:08 -0600

    fix(styles): :bug: se corrigen errores
    se corrigen importaciones y llamadas al pallete en el componente button

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import type { App } from "vue";
-import NvButton from "./components/Button/NvButton.vue";
+import type { App } from 'vue';
+import NvButton from './components/Button/NvButton.vue';
 
 export default {
   install(app: App) {
-    app.component("nv-button", NvButton);
+    app.component('nv-button', NvButton);
   },
 };
 
