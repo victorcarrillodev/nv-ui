44c3e22ca917eb36a36a58475fb5def0ef411116
Victor Carrillo <gatitto079@gmail.com>
2025-04-10 16:09:48 -0600

    test(theme): :construction: se hacen pruebas
    Se realizan pruebas con el nuevo tema propuesto

--- src/theme/constants/theme-keys.ts
+++ src/theme/constants/theme-keys.ts
@@ -1,10 +1,5 @@
 // src/theme/constants/theme-keys.ts
-
+import type { ThemeContext } from '@/theme/types/theme-provider';
 import type { InjectionKey } from 'vue';
-import type { ThemeContext } from '../types/theme-provider';
 
-/**
- * Clave de inyección del contexto del tema.
- * Se usa para proveer e inyectar el tema globalmente en la app.
- */
-export const ThemeSymbol: InjectionKey<ThemeContext> = Symbol('theme');
+export const ThemeSymbol: InjectionKey<ThemeContext> = Symbol('Theme');
