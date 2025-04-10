10c73659fc30f0eb20d01b5806c9202579f5ce32
Victor Carrillo <gatitto079@gmail.com>
2025-04-10 16:25:30 -0600

    fix: :art: se realiza primera prueba
    Se realiza primer prueba del nuevo tema creado, aparentemente todo va bien

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,10 @@
 <script setup lang="ts">
 import ThemeProvider from '@/theme/providers/ThemeProvider.vue';
-// import { useTheme } from '@/theme/composables/useTheme/index';
-// import NvButton from './components/Button/UIButton.vue';
-
-// Componente hijo que usa el tema
-// const ThemedComponent = {
-//   setup() {
-//     const { theme, toggleMode } = useTheme();
-
-//     return {
-//       theme,
-//       toggleMode,
-//     };
-//   },
-//   template: `
-//     <div :style="{
-//       backgroundColor: theme.pallete.background.paper,
-//       color: theme.colors.text,
-//       padding: '20px'
-//     }">
-//       <p>Current mode: {{ theme.mode }}</p>
-//       <button @click="toggleMode">Toggle Theme</button>
-//     </div>
-//   `,
-// };
+import ThemedMove from './ThemedMove.vue';
 </script>
 
 <template>
   <ThemeProvider>
-    <h2>ola perras</h2>
-    <!-- <ThemedComponent /> -->
-    <!-- <h2>default component</h2>
-    <NvButton>default button</NvButton>
-    <h2>color component</h2>
-    <NvButton color="primary" variant="outlined" shape="rounded">color primary</NvButton>
-    <NvButton color="secondary">vayase alv mi compa</NvButton> -->
+    <ThemedMove />
   </ThemeProvider>
 </template>
