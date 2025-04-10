d17847927327931a71e506507dcabc2e5f7e6580
Victor Carrillo <gatitto079@gmail.com>
2025-04-10 16:25:30 -0600

    fix: :art: se realiza primera prueba
    Se realiza primer prueba del nuevo tema creado, aparentemente todo va bien

---
+++ src/ThemedMove.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useTheme } from '@/theme/composables/useTheme';
+
+const { theme, toggleMode } = useTheme();
+</script>
+
+<template>
+  <div>
+    <h1>Modo actual: {{ theme.palette.mode }}</h1>
+    <button @click="toggleMode">Cambiar tema</button>
+  </div>
+</template>
