4441059494fc81b8d8b7c290b4b1756442aad46e
Victor Carrillo <gatitto079@gmail.com>
2025-04-13 02:06:15 -0600

    feat(prop): :sparkles: se agrega nueva prop
    Se agrega nueva prop "shadow" para el componente Button

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,8 @@ import { NvButton } from '.';
 <template>
   <ThemeProvider>
     <ThemedMove />
-    <NvButton color="primary">button 1</NvButton>
-    <NvButton color="error">button 2</NvButton>
+    <NvButton color="primary" shadow="5">button 1</NvButton>
+    <NvButton color="error" shadow="12">button 2</NvButton>
     <NvButton color="secondary">button 2</NvButton>
     <NvButton color="success" size="sm">button 2</NvButton>
   </ThemeProvider>
