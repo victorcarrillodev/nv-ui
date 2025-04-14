2d96d20cae1de06f5a23c4a6eeefda0070170366
Victor Carrillo <gatitto079@gmail.com>
2025-04-14 02:12:26 -0600

    chore: release v0.21.1

--- src/components/Button/button.d.ts
+++ src/components/Button/button.d.ts
@@ -18,7 +18,7 @@ export interface ButtonClassesOptions {
   variant: ButtonVariant;
   size: ButtonSizes;
   color: ButtonColors;
-  className: string;
+  className?: string;
   shape: ButtonShapes;
   shadow: number | string;
 }
