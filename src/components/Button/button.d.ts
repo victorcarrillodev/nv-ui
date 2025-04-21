befc7fc3184db2207459f11d9131a48a598b5a28
Victor Carrillo <gatitto079@gmail.com>
2025-04-21 17:30:43 -0600

    fix(types): :label: se arreglan types
    Se arreglan types para el componente Button

--- src/components/Button/button.d.ts
+++ src/components/Button/button.d.ts
@@ -1,23 +1,24 @@
-// Definiciones de tipos para el componente Button
-
 export type ButtonVariant = 'filled' | 'outlined' | 'text';
+export type ButtonSize = 'sm' | 'md' | 'lg';
+export type ButtonColor = 'primary' | 'secondary' | 'success' | 'info' | 'error' | 'warning';
+export type ButtonShape = 'normal' | 'rounded' | 'pill';
 
 export interface ButtonProps {
   disabled?: boolean;
   variant?: ButtonVariant;
-  size?: 'sm' | 'md' | 'lg';
-  color?: 'primary' | 'secondary' | 'success' | 'info' | 'error' | 'warning';
-  shape?: 'normal' | 'rounded' | 'pill';
+  size?: ButtonSize;
+  color?: ButtonColor;
+  shape?: ButtonShape;
   shadow?: number | string;
 }
 
 export interface ButtonClassesOptions {
   disabled: boolean;
   variant: ButtonVariant;
-  size: 'sm' | 'md' | 'lg';
-  color: 'primary' | 'secondary' | 'success' | 'info' | 'error' | 'warning';
+  size: ButtonSize;
+  color: ButtonColor;
   className?: string;
-  shape: 'normal' | 'rounded' | 'pill';
+  shape: ButtonShape;
   shadow: number | string;
 }
 
